241d4a2af60a310b787ece79855db2858a945635
JianB <79104723+BeerensJian@users.noreply.github.com>
2023-01-07 15:04:52 +0100

    Link to catogory route + extra styling

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
 import ProductCard from "../product-card/product-card.component";
 import "./category-preview.styles.scss";
 
 const CategoryPreview = ({ title, products }) => (
-  <div className='category-preview-container'>
-    <h2>
-      <span className='title'>{title.toUpperCase()}</span>
-    </h2>
-    <div className='preview'>
+  <div className="category-preview-container">
+    <Link className="title-link" to={title}>
+      <h2 className="title">{title.toUpperCase()}</h2>
+    </Link>
+
+    <div className="preview">
       {products
         .filter((_, index) => index < 4)
         .map((product) => (
